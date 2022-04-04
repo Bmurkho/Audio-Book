f58e8d70def3402dbb37baaaad34dcbe65254c8c
Pulok Rehan <41920280+Bmurkho@users.noreply.github.com>
2022-04-04 12:05:48 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import pyttsx3
+import PyPDF2
+
+book= open('TheArtOfExploitation.pdf', 'rb')
+pdfReader= PyPDF2.PdfFileReader(book)
+
+pages= pdfReader.numPages
+print(pages)
+
+speaker= pyttsx3.init()
+
+for num in range(14, pages):
+    page= pdfReader.getPage(14)
+    text= page.extractText()
+
+    speaker.say(text)
+    speaker.runAndWait()
